946060e4bcb9f598a5fda5a078a16652b3d3c524
wendy-schuman <wendy@selfbook.com>
2025-07-30 16:01:45 -0500

    Update streamlit_app.py
    fixed parentheses

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if time_to_insert:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response).json())
+st.text(smoothiefroot_response.json())
