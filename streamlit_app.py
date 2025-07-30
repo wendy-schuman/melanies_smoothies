e8ccaa8fa2a0ddefc55fac065a0eb3ca150078b6
wendy-schuman <wendy@selfbook.com>
2025-07-30 15:57:43 -0500

    Update streamlit_app.py
    added import for smoothiefroot

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
 st.write(
