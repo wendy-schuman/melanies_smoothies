38495f49ca4364823b2ea0272bd07d3417c0f867
wendy-schuman <wendy@selfbook.com>
2025-07-30 16:03:51 -0500

    Update streamlit_app.py
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,5 @@ if time_to_insert:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
