68833f1889145e77798fa3b28ec141caffbf6568
wendy-schuman <wendy@selfbook.com>
2025-07-30 16:10:01 -0500

    Update streamlit_app.py
    more fruityvice changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,6 @@ if time_to_insert:
 
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+st.text(fruityvice_response.json())
+sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
