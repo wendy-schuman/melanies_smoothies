b2f02ad5612826b4e4c35e2a5c3154fb4b9607e6
wendy-schuman <wendy@selfbook.com>
2025-07-30 15:59:53 -0500

    Update streamlit_app.py
    moved the request to the bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -43,3 +39,7 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered ' + name_on_order + '!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
